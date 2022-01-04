9bc28467ed6a23ff34f985c254c42ac804c7ffa2
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-01-04 08:55:00 -0300

    feat: adding new classes (Dollar and Euro)
    feat: adding new classes (Dollar and Euro)

--- src/money.ts
+++ src/money.ts
@@ -1,10 +1,41 @@
-export class Money {
+export abstract class Money {
 	public amount: number;
+
+	static dollar(amount: number): Dollar {
+		return new Dollar(amount);
+	}
+	static euro(amount: number): Euro {
+		return new Euro(amount);
+	}
 
 	equals(other: Money): boolean {
 		if (this.constructor !== other.constructor) {
 			return false;
 		}
 		return this.amount === other.amount;
+	}
+
+	abstract times(multiplier: number): Money;
+}
+
+export class Dollar extends Money {
+	constructor(amount: number) {
+		super();
+		super.amount = amount;
+	}
+
+	times(multiplier: number): Dollar {
+		return new Dollar(this.amount * multiplier);
+	}
+}
+
+export class Euro extends Money {
+	constructor(amount: number) {
+		super();
+		super.amount = amount;
+	}
+
+	times(multiplier: number): Euro {
+		return new Euro(this.amount * multiplier);
 	}
 }
