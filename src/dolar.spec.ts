f177ad44110a7bb1ae5b505112118d02fa4922d5
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-01-04 08:24:50 -0300

    test: new test for euro class
    test: new test for euro class

--- src/dolar.spec.ts
+++ src/dolar.spec.ts
@@ -1,10 +1,17 @@
 import { Dollar } from './dolar';
+import { Euro } from './euro';
 
 describe('Dollar', () => {
-	it('should return 10 when five dollars are mutiplied by two', () => {
+	it('should return 10 when five dollars are mutiplied by two and 15 when mutiplied by three', () => {
 		const five: Dollar = new Dollar(5);
 		expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
 		expect(five.times(3).equals(new Dollar(15))).toBeTruthy();
+	});
+
+	it('should return 10 when five euros are mutiplied by two and 15 when mutiplied by three', () => {
+		const five: Euro = new Euro(5);
+		expect(five.times(2).equals(new Euro(10))).toBeTruthy();
+		expect(five.times(3).equals(new Euro(15))).toBeTruthy();
 	});
 
 	it('should return true when dollars of the same value are compared', () => {
