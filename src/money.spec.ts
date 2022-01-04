b9300220e893ad8672f65d008a1ffb78d3d8d88a
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-01-04 08:41:28 -0300

    test: compare euro with dollar
    test: compare euro with dollar

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -2,23 +2,25 @@ import { Dollar } from './dolar';
 import { Euro } from './euro';
 
 describe('Money', () => {
-	it('should return 10 when five dollars are mutiplied by two and 15 when mutiplied by three', () => {
+	it('should correctly handle dollar multiplication', () => {
 		const five: Dollar = new Dollar(5);
 		expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
 		expect(five.times(3).equals(new Dollar(15))).toBeTruthy();
 	});
 
-	it('should return 10 when five euros are mutiplied by two and 15 when mutiplied by three', () => {
+	it('should correctly handle euro multiplication', () => {
 		const five: Euro = new Euro(5);
 		expect(five.times(2).equals(new Euro(10))).toBeTruthy();
 		expect(five.times(3).equals(new Euro(15))).toBeTruthy();
 	});
 
-	it('should return true when dollars and euros of the same value are compared', () => {
+	it('should correctly handle money equality', () => {
 		expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
 		expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
 
 		expect(new Euro(5).equals(new Euro(5))).toBeTruthy();
 		expect(new Euro(5).equals(new Euro(6))).toBeFalsy();
+
+		expect(new Euro(5).equals(new Dollar(5))).toBeFalsy();
 	});
 });
