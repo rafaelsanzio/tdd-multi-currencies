f7123370b8cc3fd8132f48e895abc6270eba2f3b
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-01-05 09:00:21 -0300

    feat: implement plus method to handle addition
    feat: implement plus method to handle addition

--- src/money.ts
+++ src/money.ts
@@ -1,4 +1,6 @@
-export class Money {
+import { Expression } from './expression';
+
+export class Money implements Expression {
 	private readonly amount: number;
 	private readonly _currency: string;
 
@@ -24,6 +26,11 @@ export class Money {
 	times(multiplier: number): Money {
 		return new Money(this.amount * multiplier, this._currency);
 	}
+
+	plus(addend: Money): Expression {
+		return new Money(this.amount + addend.amount, this._currency);
+	}
+
 	currency(): string {
 		return this._currency;
 	}
