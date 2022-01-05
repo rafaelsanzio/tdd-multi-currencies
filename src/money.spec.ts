870d678a5c8a7edf8efda3e34aaf0987c25b3b26
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-01-05 08:43:24 -0300

    refactor: remove redundant tests
    refactor: remove redundant tests

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -7,22 +7,11 @@ describe('Money', () => {
 		expect(five.times(3).equals(Money.dollar(15))).toBeTruthy();
 	});
 
-	it('should correctly handle euro multiplication', () => {
-		const five: Money = Money.euro(5);
-		expect(five.times(2).equals(Money.euro(10))).toBeTruthy();
-		expect(five.times(3).equals(Money.euro(15))).toBeTruthy();
-	});
-
 	it('should correctly handle money equality', () => {
 		expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
 		expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
 
-		expect(Money.euro(5).equals(Money.euro(5))).toBeTruthy();
-		expect(Money.euro(5).equals(Money.euro(6))).toBeFalsy();
-
 		expect(Money.euro(5).equals(Money.dollar(5))).toBeFalsy();
-
-		expect(new Money(5, 'USD').equals(Money.dollar(5))).toBeTruthy();
 	});
 
 	it('should correctly handle currencies', () => {
