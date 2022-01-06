1f6edfb2981731efd0a829b22bdb4a12cffc4333
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-01-06 09:11:20 -0300

    feat: implement new interface Expression
    feat: implement new interface Expression

--- src/expression.ts
+++ src/expression.ts
@@ -1,2 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Expression {}
+import { Money } from './money';
+export interface Expression {
+	reduce(to: string): Money;
+}
