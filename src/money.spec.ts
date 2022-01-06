d65a0465f0a14539ac9d66cbafce23e804ac9969
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-01-06 09:11:44 -0300

    test: new tests for sum class
    test: new tests for sum class

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -1,6 +1,7 @@
 import { Money } from './money';
 import { Expression } from './expression';
 import { Bank } from './bank';
+import { Sum } from './sum';
 
 describe('Money', () => {
 	it('should handle dollar multiplication', () => {
@@ -27,5 +28,27 @@ describe('Money', () => {
 		const bank = new Bank();
 		const reduced: Money = bank.reduce(sum, 'USD');
 		expect(reduced.equals(Money.dollar(10))).toBeTruthy();
+	});
+
+	it('should return a sum when plus is used', () => {
+		const five = Money.dollar(5);
+		const result: Expression = five.plus(five);
+		const sum: Sum = result as Sum;
+		expect(sum.augend).toEqual(five);
+		expect(sum.addend).toEqual(five);
+	});
+
+	it('should reduce a sum', () => {
+		const sum: Expression = new Sum(Money.dollar(3), Money.dollar(4));
+		const bank = new Bank();
+		const result: Money = bank.reduce(sum, 'USD');
+		expect(result).toEqual(Money.dollar(7));
+	});
+
+	it('should reduce from a money', () => {
+		const bank = new Bank();
+		//bank.addRate('USD', 'EUR', 2);
+		const result: Money = bank.reduce(Money.dollar(1), 'EUR');
+		expect(result).toEqual(Money.dollar(1));
 	});
 });
